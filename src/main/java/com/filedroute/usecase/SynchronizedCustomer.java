680e3efb3a7de91ef0ff60efc7e90afb89f1b906
Renato <serra.renato@gmail.com>
2023-04-01 22:30:46 -0300

    Melhorias no codigo

--- src/main/java/com/filedroute/usecase/SynchronizedCustomer.java
+++ src/main/java/com/filedroute/usecase/SynchronizedCustomer.java
@@ -4,56 +4,56 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.filedroute.entity.CustomerLocal;
 import com.filedroute.entity.CustomerRemote;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class SynchronizedCustomer {
     ObjectMapper oMapper = new ObjectMapper();
-    public Map<String, List<Map<String, String>>> synchronizeCustomer(List<CustomerLocal> customerLocal, List<CustomerRemote> customerRemote){
-        Map<String, List<Map<String, String>>>  result = new HashMap<>();
+
+    public Map<String, List<Map<String, String>>> synchronizeCustomer(List<CustomerLocal> listCustomerLocal, List<CustomerRemote> listCustomerRemote) {
+        Map<String, List<Map<String, String>>> result = new HashMap<>();
         List<Map<String, String>> localUpdates = new ArrayList<>();
         List<Map<String, String>> remoteUpdates = new ArrayList<>();
-        for (CustomerLocal local:customerLocal) {
+        for (CustomerLocal local : listCustomerLocal) {
             Long id = local.getCustomerID();
             boolean found = false;
-            for (CustomerRemote remote : customerRemote) {
-                if (id.equals(remote.getCustomerNumber())) {
-                    found = true;
-                    if (local.getDateUpdated().isAfter(remote.getDateUpdated())) {
-                        Map<String, String> remoteUpdate = new HashMap<>();
-                        remoteUpdate = oMapper.convertValue(local, Map.class);
-                        remoteUpdates.add(remoteUpdate);
-                    } else {
-                        Map<String, String> localUpdate = new HashMap<>();
-                        localUpdate = oMapper.convertValue(remote, Map.class);
-                        localUpdates.add(localUpdate);
-                    }
+            //Optional<U> remoteObjectOptional = remoteObjects.stream().filter(remoteObject -> remoteIdFunction.apply(remoteObject).equals(id)).findFirst();
+            Optional<CustomerRemote> customerRemote = listCustomerRemote.stream().filter(x -> x.getCustomerNumber().equals(id)).findFirst();
+            //  for (CustomerRemote remote : customerRemote) {
+            if (customerRemote.isPresent()) {
+                found = true;
+                if (local.getDateUpdated().isAfter(customerRemote.get().getDateUpdated())) {
+                    Map<String, String> remoteUpdate = new HashMap<>();
+                    remoteUpdate = oMapper.convertValue(local, Map.class);
+                    remoteUpdates.add(remoteUpdate);
+                } else {
+                    Map<String, String> localUpdate = new HashMap<>();
+                    localUpdate = oMapper.convertValue(customerRemote.get(), Map.class);
+                    localUpdates.add(localUpdate);
                 }
             }
-            if (!found){
+            //  }
+            if (!found) {
                 Map<String, String> remoteUpdate = new HashMap<>();
                 remoteUpdate = oMapper.convertValue(local, Map.class);
                 remoteUpdates.add(remoteUpdate);
             }
         }
-        for (CustomerRemote remote : customerRemote) {
+        for (CustomerRemote remote : listCustomerRemote) {
             Long id = remote.getCustomerNumber();
             boolean found = false;
-            for (CustomerLocal local:customerLocal) {
-                if (id.equals(local.getCustomerID())) {
-                    found=true;
-                }
+            Optional<CustomerLocal> local = listCustomerLocal.stream().filter(x -> x.getCustomerID().equals(id)).findFirst();
+            //for (CustomerLocal local:listCustomerLocal) {
+            if (local.isPresent()) {
+                found = true;
             }
-            if (!found){
-                Map<String, String> localUpdate = new HashMap<>();
-                localUpdate = oMapper.convertValue(remote, Map.class);
+            //}
+            if (!found) {
+                Map<String, String> localUpdate = oMapper.convertValue(remote, Map.class);
                 localUpdates.add(localUpdate);
             }
         }
-        result.put("Local Updates",localUpdates);
-        result.put("Remote Updates",remoteUpdates);
+        result.put("Local Updates", localUpdates);
+        result.put("Remote Updates", remoteUpdates);
 
         return result;
     }
